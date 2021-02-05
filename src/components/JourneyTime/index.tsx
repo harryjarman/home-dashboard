082f8fee77ebe791de25e2bfc3dbbac2d11aad10
Harry Jarman <harry.jarman@bbc.co.uk>
2021-02-05 15:31:49 +0000

    Journey estimate

--- src/components/JourneyTime/index.tsx
+++ src/components/JourneyTime/index.tsx
@@ -4,11 +4,19 @@ import styles from "./JourneyTime.module.scss";
 
 const JourneyTime = () => {
   const [journeyTime, setJourneyTime] = useState("Loading...");
+  const [stationInfo, setStationInfo] = useState({
+    stationFrom: "",
+    stationTo: "",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get("/api/tfl_journey");
       setJourneyTime(data.journey_time + "m");
+      setStationInfo({
+        stationFrom: data.stationFrom,
+        stationTo: data.stationTo,
+      });
     };
     fetchData();
 
@@ -21,9 +29,15 @@ const JourneyTime = () => {
   }, []);
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <div className={styles.large}>{journeyTime}</div>
       <div className={styles.caption}>Estimated time to TCR</div>
+      <a
+        href={`https://tfl.gov.uk/plan-a-journey/results?FromId=${stationInfo.stationFrom}&ToId=${stationInfo.stationTo}`}
+        target="_blank"
+      >
+        Journey Details
+      </a>
     </div>
   );
 };
--- src/pages/api/tfl_journey.ts
+++ src/pages/api/tfl_journey.ts
@@ -1,10 +1,10 @@
 export default async (req, res) => {
   switch (req.method) {
     case "GET": {
-      const stationCodeStratford = "1000226";
-      const stationCodeTCR = "1000235";
+      const stationFrom = process.env.JRNY_EST_FROM;
+      const stationTo = process.env.JRNY_EST_TO;
       const journeyResponse = await fetch(
-        `https://api.tfl.gov.uk/journey/journeyresults/${stationCodeStratford}/to/${stationCodeTCR}?app_id=${process.env.TFL_APP_ID}&app_key=${process.env.TFL_PRIMARY_KEY}`
+        `https://api.tfl.gov.uk/journey/journeyresults/${stationFrom}/to/${stationTo}?app_id=${process.env.TFL_APP_ID}&app_key=${process.env.TFL_PRIMARY_KEY}`
       );
       const journeyDetails = await journeyResponse.json();
       const journey_time = Math.ceil(
@@ -12,7 +12,12 @@ export default async (req, res) => {
           return aggr + value.duration;
         }, 0) / journeyDetails.journeys.length
       );
-      res.json({ tflResponse: journeyDetails, journey_time });
+      res.json({
+        tflResponse: journeyDetails,
+        journey_time,
+        stationFrom,
+        stationTo,
+      });
       break;
     }
     default: {
